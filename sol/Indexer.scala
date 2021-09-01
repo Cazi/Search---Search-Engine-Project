7c8863ed2dae76219259434949609835a36338c6
Cazi <71891696+Cazi@users.noreply.github.com>
2021-09-01 12:00:30 -0400

    Updated the indexer file to reflect new parsing algorithm.

--- sol/Indexer.scala
+++ sol/Indexer.scala
@@ -2,94 +2,109 @@ package sol
 
 import src.{FileIO, PorterStemmer, StopWords}
 
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.HashSet
-import scala.reflect.internal.util.Collections
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.util.matching.Regex
 import scala.xml.{Node, NodeSeq}
-
+import math.BigDecimal
 /**
  * Provides an XML indexer, produces files for a querier
  *
  * @param inputFile - the filename of the XML wiki to be indexed
  */
-class Indexer(val inputFile: String) {
-  //These variables are needed to compute term frequency
-  //Number of pages in the corpus or n
-  var numOfPages = 0.0
-  //Number of words in the corpus
-  var numOfWords = 0
+class Indexer(val inputFile: String, val inputFile2: String, val inputFile3: String,
+              val inputFile4: String) {
+  val tempLinksMap = new mutable.HashMap[Int, mutable.HashSet[String]]()
+  val linksMap = new mutable.HashMap[Int, mutable.HashSet[Int]]()
+  val idToTitle = new mutable.HashMap[Int, String]()
+  val titleToId = new mutable.HashMap[String, Int]()
+  val rMap = new mutable.HashMap[Int, Double]()
+  val rPrimeMap = new mutable.HashMap[Int, Double]()
+  var wordsMap = new mutable.HashMap[String, mutable.HashMap[Int, Double]]()
+  val pageSeq: NodeSeq = xml.XML.loadFile(inputFile) \ "page"
+  val numOfPages = pageSeq.size
 
-  //These HashMaps are used to compute page rank
-  //Unranked documents in the corpus
-  var rMap = new HashMap[Int, Double]()
-  //Corpus documents ranked with PageRank
-  var rPrimeMap = new HashMap[Int, Double]()
-  //All page ids mapped to the pages they link to
-  var linksMap = new HashMap[Int, List[String]]()
+  parse(inputFile, inputFile2, inputFile3, inputFile4)
 
-  //Theses HashMaps link the titles and ids to each other in two HashMaps
-  var idToTitle = new HashMap[Int, String]()
-  var titleToId = new HashMap[String, Int]()
-
-  // List of Ids and Titles
-  var idList: List[Int] = idToTitle.keys.toList
-  var titlesList: List[String] = titleToId.keys.toList
-  //HashMap of words -> (page Id -> Term Frequency)
-  var wordsMap = new HashMap[String, HashMap[Int, Double]]()
-
-  //Accessing the xml file
-  //Node containing entire input file
-  val root: Node = xml.XML.loadFile(inputFile)
-  //Sequence of pages extracted from the root node
-  val pageSeq: NodeSeq = root \ "page"
-  //Sequence of ids extracted from the root node
-  val idSeq: NodeSeq = root \ "page" \ "id"
-  //Sequence of titles extracted from the root node
-  val titleSeq: NodeSeq = root \ "page" \ "title"
-
-  //Begin indexing and page rank
-  getTitlesIds()
-  parse(inputFile)
-  relevance()
-  pageRank()
-
-  def getTitlesIds(): Unit = {
+  /**
+   * parse method processes the corpus
+   *
+   * @param inputFile  - xml file
+   * @param inputFile2 - titles.txt
+   * @param inputFile3 - docs.txt
+   * @param inputFile4 - words.txt
+   */
+  def parse(inputFile: String, inputFile2: String, inputFile3: String,
+            inputFile4: String): Unit = {
     for (page <- pageSeq) {
-      val pageTitle = (page \ "title").text.trim
-      val pageId = (page \ "id").text.trim.toInt
-      //Store ID to title
-      idToTitle.put(pageId, pageTitle)
-      //Store title to ID
-      titleToId.put(pageTitle, pageId)
+      processPage(page)
     }
-  }
 
+    wordsMap = relevance(numOfPages)
+    processLinks()
+    pageRank()
+    FileIO.writeTitlesFile(inputFile2, idToTitle)
+    FileIO.writeDocsFile(inputFile3, rPrimeMap)
+    FileIO.writeWordsFile(inputFile4, wordsMap)
+  }
 
   /**
    *
-   * @param link
-   * @param pageId
-   * @return
+   * @param page - Current page
+   */
+  def processPage(page: Node): Unit = {
+    val pageId = (page \ "id").text.trim.toInt
+    val pageTitle = (page \ "title").text.trim
+    val pageText = (page \ "text").text.trim
+    idToTitle.put(pageId, pageTitle)
+    titleToId.put(pageTitle, pageId)
+    linksMap.put(pageId, new mutable.HashSet[Int])
+
+    val genRegex = new Regex("""\[\[[^\[]+?\]\]|[^\W_]+'[^\W_]+|[^\W_]+""")
+    val matchesIterator = genRegex.findAllMatchIn(pageTitle + pageText)
+    val matchesList = matchesIterator.toList.map(aMatch => aMatch.matched)
+    val allText = textLinkArrays(matchesList, pageId)
+    val wordList = allText
+
+    for (word <- wordList) {
+      if (wordsMap.keys.toList.contains(word)) {
+        if (wordsMap(word).contains(pageId)) {
+          wordsMap(word)(pageId) += 1
+        } else {
+          wordsMap(word).put(pageId, 1)
+        }
+      } else {
+        wordsMap.put(word, new mutable.HashMap(pageId, 1))
+      }
+    }
+    rMap.put(pageId, 0.0)
+    rPrimeMap.put(pageId, 1.0 / numOfPages)
+  }
+
+  /**
+   * getLinkText extracts the page text from a link
+   *
+   * @param link - The current link
+   * @return The list of words in the link's text
    */
   def getLinkText(link: String): List[String] = {
     if (link.contains("|")) {
-      return link.substring(link.indexOf("|") + 1, link.length - 2).split(" ").toList
+      link.substring(link.indexOf("|") + 1, link.length - 2).split(" ").toList
     } else if (link.contains(":")) {
       val noBrackets = link.substring(2, link.length - 2)
-      return noBrackets.substring(0, link.indexOf(":") - 1).split(" ").toList :::
+      noBrackets.substring(0, link.indexOf(":") - 1).split(" ").toList :::
         noBrackets.substring(noBrackets.indexOf(":") + 1, noBrackets.length).split(" ").toList
-    } else if (link.contains("[")) {
+    } else {
       val noBrackets = link.substring(2, link.length - 2).split(" ").toList
-      return noBrackets
+      noBrackets
     }
-    List()
   }
 
   /**
+   * getLinkTitle extracts the page title from a link
    *
-   * @param link
-   * @return
+   * @param link - The current link
+   * @return The title of page being linked to
    */
   def getLinkTitle(link: String): String = {
     if (link.contains("|")) {
@@ -100,161 +115,136 @@ class Indexer(val inputFile: String) {
     link.trim
   }
 
-  private def textLinkArrays(allWords: List[String]): (List[String], List[String]) = {
-    var words: List[String] = List()
-    var links: List[String] = List()
+  /**
+   * textLinkArrays processes the words and links in the corpus
+   *
+   * @param allWords - All the unprocessed words and links
+   * @return A list of all processed words
+   */
+  def textLinkArrays(allWords: List[String], pageId: Int):
+  List[String] = {
+    val words: ListBuffer[String] = ListBuffer()
+    val links: ListBuffer[String] = ListBuffer()
     for (word <- allWords) {
-      //Don't change link titles when putting it in the links map, do lower case, stem, etc, on page link text afterwards
       if (word.matches("""\[\[[^\[]+?\]\]""")) {
-        links = getLinkTitle(word) :: links
+        val linkTitle = getLinkTitle(word)
+        links += linkTitle
         val linkText = getLinkText(word.toLowerCase)
-        words = words ::: PorterStemmer.stemArray(linkText.filterNot(x => StopWords.isStopWord(x)).toArray).toList
+        val newWords =
+          PorterStemmer.stemArray(linkText.filterNot(x => StopWords.isStopWord(x)).toArray).toList
+        words.concat(newWords)
       } else {
         if (!StopWords.isStopWord(word)) {
-          words = PorterStemmer.stem(word.toLowerCase) :: words
+          words += PorterStemmer.stem(word.toLowerCase)
         }
       }
     }
-    //println(links)
-    (words, links)
-
+    tempLinksMap.put(pageId, mutable.HashSet[String]())
+    for (link <- links) {
+      tempLinksMap(pageId).add(link)
+    }
+    words.toList
   }
 
-
   /**
-   * Parse method constructs [insert structures here]
-   *
-   * @param inputFile
+   * Process links takes the temporary link structure and stores the correct
+   * link structure now that titleToId has been filled
    */
-  def parse(inputFile: String): Unit = {
-    for (page <- pageSeq) {
-      numOfPages += 1
-      //Grab the id, title, and text from the current page
-      val pageId = (page \ "id").text.trim.toInt
-      val pageTitle = (page \ "title").text.trim
-      val pageText = (page \ "text").text.trim
-
-      //Extract all the words and links from the pages
-      val genRegex = new Regex("""\[\[[^\[]+?\]\]|[^\W_]+'[^\W_]+|[^\W_]+""")
-      val matchesIterator = genRegex.findAllMatchIn(pageTitle + pageText)
-      val matchesList = matchesIterator.toList.map(aMatch => aMatch.matched)
-      val allText = textLinkArrays(matchesList)
-      val wordList = allText._1
-      val linksList = allText._2
-      linksMap.put(pageId, linksList)
-
-      for (word <- wordList) {
-        if (wordsMap.keys.toList.contains(word)) {
-          if(wordsMap(word).contains(pageId)) {
-            wordsMap(word)(pageId) += 1
-          }
-          wordsMap(word).put(pageId, 1)
-        }
-        wordsMap.put(word, new HashMap(pageId, 1))
-      }
-    }
-  }
-
-  //Calculate relevance and store it in the outputWords HashMap
-  //First, calculate term frequency
-  def relevance(): Unit = {
-    val occurrenceMap = new HashMap[Int, Double]()
-    for ((_, map) <- wordsMap) {
-      for (id <- map.keys) {
-        if (occurrenceMap.keys.toList.contains(id)) {
-          if (map(id) > occurrenceMap(id)) {
-            occurrenceMap.update(id, map(id))
-          }
-          occurrenceMap.put(id, map(id))
-        }
-      }
-    }
-    //Using term frequency and inverse document frequency,
-    // calculate relevance
-    for ((word, map) <- wordsMap) {
-      for (id <- map.keys) {
-        if (map.keys.toList.contains(id) && occurrenceMap.keys.toList.contains(id)) {
-          map.update(id,
-            (map(id) / occurrenceMap(id)) * Math.log(numOfPages / wordsMap(word).size))
+  def processLinks(): Unit = {
+    for ((page, lTitles) <- tempLinksMap) {
+      for (link <- lTitles) {
+        if (titleToId.contains(link)) {
+          linksMap(page).add(titleToId(link))
         }
       }
     }
   }
 
   /**
-   * Calculates the weight that k (from) give to j (to)'s rank
+   * Weight calculates the weight that k (from) give to j (to)'s rank
    *
    * @param fromK - Page k
    * @param toJ   - Page j
    */
   def weight(fromK: Integer, toJ: Integer): Double = {
-
-    if (fromK == toJ) { //k equals j
-      println("k equals j " + fromK)
-      return .15 / numOfPages
-    } else if (linksMap(fromK).contains(idToTitle(toJ))) {
-      //k links to j
-      println("k links to j " + fromK)
-      return .15 / numOfPages + (.85 / linksMap(fromK).distinct.size.toDouble)
-    } else if (linksMap(fromK).isEmpty) {
-      println("k links to nothing " + fromK)
-      //k links to nothing
-      return .15 / numOfPages + (.85 / (numOfPages - 1))
+    var weight = 0.0
+    val epsilon = .15
+    val default = epsilon / numOfPages
+    if (linksMap(fromK).isEmpty) {
+      weight = default + (.85 / (numOfPages.toDouble - 1.0))
+      weight
+    } else if (fromK == toJ) {
+      weight = default
+      weight
+    } else if (linksMap(fromK).contains(toJ)) {
+      weight = default + (.85 / linksMap(fromK).size.toDouble)
+      weight
     } else {
-      println("else case " + fromK)
-      //all other cases
-      .15 / numOfPages
+      weight = default
+      weight
     }
   }
 
   /**
-   * Page rank helper that fills a given HashMap with a certain value
-   *
-   * @param value
-   * @param from
-   * @param to
-   */
-  def fillHashMap(value: Double, from: HashMap[Int, String],
-                  to: HashMap[Int, Double]): Unit = {
-    for (ids <- from.keys.toList) {
-      to.put(ids, value)
-    }
-  }
-
-  /**
-   *
-   * @param r
-   * @param rprime
-   *
    * To calculate distance we need to get the sum of the difference between r
    * and r' on every page of the hashMap
+   *
+   * @param r      - HashMap representing the previous iteration of page rank
+   * @param rprime - HashMap representing the current iteration of page rank
    */
-  def rankDistance(r: HashMap[Int, Double],
-                   rprime: HashMap[Int, Double]): Double = {
+  def rankDistance(r: mutable.HashMap[Int, Double],
+                   rprime: mutable.HashMap[Int, Double]): Double = {
     var sum = 0.0
-    for (i <- idToTitle.keys.toList) {
-      sum += ((rprime(i) - r(i)) * (rprime(i) - r(i)))
+    for (i <- idToTitle.keys) {
+      sum += Math.pow(rprime(i) - r(i), 2)
     }
     Math.sqrt(sum)
   }
 
-  def pageRank(): Unit = {
-    fillHashMap(0.0, idToTitle, rMap)
-    fillHashMap(1.0 / numOfPages, idToTitle, rPrimeMap)
+  /**
+   * Calculate the relevance of every word in the corpus
+   *
+   * @param numPages - Number of pages in the corpus
+   * @return Words to docs to frequency hashmap
+   */
+  def relevance(numPages: Double):
+  mutable.HashMap[String, mutable.HashMap[Int, Double]] = {
+    val maxWordOccurrence = new mutable.HashMap[Int, Double]()
+    val wordFrequency = wordsMap.clone()
 
+    for ((word, idoMap) <- wordsMap) {
+      for ((id, occur) <- idoMap) {
+        if (maxWordOccurrence.keys.toList.contains(id)) {
+          if (occur > maxWordOccurrence(id)) {
+            maxWordOccurrence.update(id, occur)
+            wordFrequency(word)(id) =
+              (wordsMap(word)(id) / maxWordOccurrence(id)) *
+                Math.log(numPages / wordsMap(word).size)
+          }
+        }
+        maxWordOccurrence.put(id, occur)
+      }
+    }
+    wordFrequency
+  }
+
+  def pageRank(): Unit = {
     while (rankDistance(rMap, rPrimeMap) > .001) {
-      //check rMap updating
-      rMap = rPrimeMap.clone()
-      for (j <- idToTitle.keys.toList) {
+      for ((v, _) <- rPrimeMap) {
+        rMap(v) = rPrimeMap(v)
+      }
+      for (j <- idToTitle.keys) {
         rPrimeMap(j) = 0.0
-        for (k <- idToTitle.keys.toList) {
-          rPrimeMap(j) += weight(k, j) * rMap(k)
-          //println(weight(k, j) + " " + k + " " + j)
+        for (k <- idToTitle.keys) {
+          val weightCalc = weight(k, j) * rMap(k)
+          rPrimeMap(j) =
+            rPrimeMap(j) + weightCalc
         }
       }
     }
   }
 }
+
 
 object Indexer {
   /**
@@ -266,10 +256,9 @@ object Indexer {
     if (!(args.length == 4)) {
       throw new IllegalArgumentException("Incorrect Args Config")
     }
-    val indexer = new Indexer(args(0))
-    FileIO.writeTitlesFile(args(1), indexer.idToTitle)
-    FileIO.writeWordsFile(args(3), indexer.wordsMap)
-    FileIO.writeDocsFile(args(2), indexer.rPrimeMap)
+    new Indexer(args(0), args(1), args(2), args(3))
   }
+
+
 }
 
